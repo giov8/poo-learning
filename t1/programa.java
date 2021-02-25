04a257b0d2bd331367bb2373275d72d53fe87178
Giovani <ggm18@inf.ufpr.br>
2021-02-25 15:51:14 -0300

    iniciado

---
+++ t1/programa.java
@@ -0,0 +1,17 @@
+class Arquivo {
+	String nome, extensao, tipo;
+	float tamanho;
+}
+
+class Diretorio {
+	String nome;
+	ArrayList arquivos, subdiretorios;
+
+}
+
+public class Programa {
+	public static void main(String[] args) {
+		Diretorio pasta1 = new Diretorio;
+		pasta1.nome = "Teste";
+	}
+}
